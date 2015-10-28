1b1a8c16a0eaa0d020b4f82213e0c711b184d352
Jaskamal Kainth <kainthjaskamal@gmail.com>
2015-10-28 19:53:29 +0530

    Code - Ad Infinitum 10

---
+++ zero_xor_subsets.cpp
@@ -0,0 +1,106 @@
+/*
+ *
+ *    J1K7_7
+ * 
+ */
+// xor problem , equivalence classes , formula based
+//Ad Infinitum 10 - Math Programming Contest
+//https://www.hackerrank.com/contests/infinitum10/challenges/number-of-subsets
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <cstring>
+#include <list>
+#include <map>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+using namespace std;
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef long double        ld;
+typedef pair<int,int>      pii;
+typedef pair<ll,ll>        pll;
+typedef vector<int>        vi;
+typedef vector<long long>  vll;
+#define l(x) 		       (x << 1) + 1
+#define r(x) 		       (x << 1) + 2
+#define mid(l, r) 	       ((l + r) >> 1)
+#define mp                 make_pair
+#define pb                 push_back
+#define all(a)             a.begin(),a.end()
+#define pr(n)              printf("%d",n)
+#define s(n)               scanf("%d",&n)
+#define debug(x)	       {cerr <<#x<<" = " <<x<<"\n"; }
+#define debug2(x, y)       {cerr <<#x<<" = " <<x<<", "<<#y <<" = " <<y <<"\n";}
+#define ss                 second
+#define ff                 first
+#define m0(x) 		       memset(x,0,sizeof(x))
+#define snuke(c,itr)       for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
+const int mod=1e9+7;
+
+const ll  mx_ll   = numeric_limits<ll> :: max();
+const int mx_int  = numeric_limits<int> :: max();
+
+const long double PI = (long double)(3.1415926535897932384626433832795);
+
+inline bool ispow2(int x){return (x!=0 && (x&(x-1))==0);} //0 or 1
+
+int msb(unsigned x){ union { double a; int b[2]; }; a = x; return (b[1] >> 20) - 1023; }
+
+template<class T>
+inline void cinarr(T a, int n){ for (int i=0;i<n;++i) cin >> a[i];}
+
+inline ll gcd(ll a,ll b){ll t;while(b){a=a%b;t=a;a=b;b=t;}return a;}
+inline ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+
+inline int nextint(){ int x; scanf("%d",&x); return x; }
+inline ll nextll(){ ll x; scanf("%lld",&x); return x; }
+
+
+long long int powmod(long long int a,long long int b, long long int MOD)
+{
+    if(b==0)
+        return 1;
+    if(b==1)
+        return a;
+    long long int ans=powmod(a,b/2,MOD);
+    ans=(ans*ans)%MOD;
+    if(b%2)
+        ans=(ans*a)%MOD;
+    return ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    int t = nextint();
+    while(t--)
+    {
+        ll n = nextll();
+
+        ll pwr = powmod(2,n,mod-1)  - n % ( mod-1)  ;
+
+        pwr = ( pwr + mod-1 ) % (mod-1);
+
+        ll ans = powmod(2 , pwr , mod );
+
+        printf("%lld\n",ans);
+
+    }
+
+    return 0;
+}
+
